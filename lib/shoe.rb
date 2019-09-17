3a8e733caa3fc6f49ee491235591d3a405dc1755
Sarah Wise <skjwise@hotmail.com>
2019-09-17 21:07:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,14 +2,13 @@
 class Shoe
   
   attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   def initialize(brand)
     @brand = brand
   end
   
-  def brand
-    @brand
-  end
+  
   
   
 end
